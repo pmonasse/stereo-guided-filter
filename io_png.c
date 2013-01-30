592d5843c193311c6622b395a4d4292649214e64
Pascal Monasse <monasse@imagine.enpc.fr>
2013-01-30 10:08:03 +0100

    Fix copyright

--- io_png.c
+++ io_png.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
+ * Copyright (c) 2010-2011, Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
